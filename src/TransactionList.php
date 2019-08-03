994820ae55d61018ff86153f67bdd4a952bb74ca
Andriy Svirin <andriy.svirin@ukrinsoft.com>
2019-08-03 09:53:31 -0400

    Add Transaction List and Add basic Validator.

---
+++ src/TransactionList.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace AndrewSvirin\MT942;
+
+use AndrewSvirin\MT942\models\Transaction;
+use ArrayIterator;
+
+/**
+ * Transaction list handler.
+ * Could be extended for additional search methods in transactions.
+ *
+ * @license http://www.opensource.org/licenses/mit-license.html  MIT License
+ * @author Andrew Svirin
+ */
+class TransactionList implements \IteratorAggregate
+{
+
+   /**
+    * @var Transaction[]
+    */
+   private $transactions = [];
+
+   /**
+    * {@inheritDoc}
+    */
+   public function getIterator(): ArrayIterator
+   {
+      return new ArrayIterator($this->transactions);
+   }
+
+   /**
+    * Get amount of transactions.
+    * @return int
+    */
+   public function count(): int
+   {
+      return count($this->transactions);
+   }
+
+   /**
+    * Add one transaction to list.
+    * @param Transaction $transaction
+    */
+   public function add(Transaction $transaction)
+   {
+      $this->transactions[] = $transaction;
+   }
+}
